e173a5898086564346834def15cd8d5eb04497b5
hulen20@gmail.com <UzE9M8JGL8I6>
2015-10-21 18:04:40 +0800

    no message

--- js/tools.js
+++ js/tools.js
@@ -1,3 +1,28 @@
-(function  (win,doc) {
-	
-})(window,document)
+;(function  ($) {
+	$.tools = function  (el,options) {
+		var $tools = $(el);
+		$tools.methods = {
+			options:{
+				device:['768','992','1200']
+			},
+			init:function  () {
+				$tools.methods.run();
+			},
+			run:function  () {
+				console.log('run');
+			}
+		}
+
+		$tools.marquee = function  (el,options) {
+			this.el = $(el);
+		}
+
+		return $tools;
+	}
+})(jQuery);
+
+var $tools = null;
+$(function  () {
+	$tools = new $.tools();
+	$tools.methods.init();
+})
