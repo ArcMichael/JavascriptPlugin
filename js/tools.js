164585f376d36fec4b73b408dfed2d5d035d2b07
hulen20@gmail.com <UzE9M8JGL8I6>
2015-10-23 13:36:08 +0800

    add fn toggle left_slider(auto)

--- js/tools.js
+++ js/tools.js
@@ -33,8 +33,16 @@
 					} catch (e) {
 						console.log('miss function && options is mistake')
 					};
-					console.log('<style type="text/css">\n\t.exampleMarquee{\n\t\twidth: 100%;\n\t\theight: 30px;\n\t\toverflow: hidden;\n\t\tbackground-color: #ccc;\n\t\t}\n\t.exampleMarquee > *{\n\t\tlist-style-type: none;\n\t\tpadding: 0px;\n\t\tmargin: 0px;\n\t}\n\t.exampleMarquee > ul li{\n\t\tfloat: left;\n\t\tpadding: 0 10px;\n\t\tline-height: 30px;\n\t}\n</style>');
-					console.log('<div class="exampleMarquee" data-action="Marquee">\n\t<ul>\n\t\t<li>postdicroticteratogenousluxuriantllej</li>\n\t\t<li>ducatocrystalloticillate</li>\n\t</ul>\n</div>')
+					// console.log('<style type="text/css">\n\t.exampleMarquee{\n\t\twidth: 100%;\n\t\theight: 30px;\n\t\toverflow: hidden;\n\t\tbackground-color: #ccc;\n\t\t}\n\t.exampleMarquee > *{\n\t\tlist-style-type: none;\n\t\tpadding: 0px;\n\t\tmargin: 0px;\n\t}\n\t.exampleMarquee > ul li{\n\t\tfloat: left;\n\t\tpadding: 0 10px;\n\t\tline-height: 30px;\n\t}\n</style>');
+					// console.log('<div class="exampleMarquee" data-action="Marquee">\n\t<ul>\n\t\t<li>postdicroticteratogenousluxuriantllej</li>\n\t\t<li>ducatocrystalloticillate</li>\n\t</ul>\n</div>')
+
+					try{
+						new $tools.toggleMenu($('[data-action=taggleMenu]'),{
+							direction:'left'
+						})
+					}catch(e){
+						console.log('miss function && options is mistake');
+					}
 
 				}
 			}
@@ -83,10 +91,10 @@
 
 			defaults.$ul.append(defaults.$li.clone()).css(defaults.scrollType ? "width" : "height", defaults.scrollSize * 2);
 
-			$tools.init(defaults);
+			$tools.marquee.init(defaults);
 		}
 
-		$tools.init = function(opts) {
+		$tools.marquee.init = function(opts) {
 			var numMoved = 0;
 
 			function scrollFunc(opts) {
@@ -127,6 +135,27 @@
 				}, opts.scrollDelay);
 			});
 		}
+
+		$tools.toggleMenu = function  (el,opts) {
+			var defaults = {
+				direction:opts.direction || 'left'
+			}
+			defaults.$el = $(el);
+			defaults.$menu = defaults.$el.children('[data-action="toggleMenunav"]');
+			defaults.$link = defaults.$el.children('a.toggle_menuLink');
+			defaults.$wrap = defaults.$el;
+			$tools.toggleMenu.init(defaults)
+		}
+		$tools.toggleMenu.init = function  (opts) {
+			// console.log(opts);
+			opts.$el.addClass('toggle_menuJs');
+			opts.$link.on('click', function() {
+				opts.$link.toggleClass('toggle_menuActive');
+				opts.$wrap.toggleClass('toggle_menuActive');
+				return false;
+			});
+		}
+
 		return $tools;
 	}
 })(jQuery);
