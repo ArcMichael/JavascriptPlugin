33a39149d81798a76515480acf4d749de5968a05
hulen20@gmail.com <UzE9M8JGL8I6>
2015-10-23 10:39:08 +0800

    typography

--- js/tools.js
+++ js/tools.js
@@ -1,51 +1,53 @@
-;(function  ($) {
-	$.tools = function  (el,opts) {
+;
+(function($) {
+	$.tools = function(el, opts) {
 		var $tools = $(el);
 		$tools.methods = {
-			init:function  () {
-				$tools.methods.D = $tools.methods.getVendorPrefix();
-				$tools.methods.run();
-			},
-			/**
-			 * [getVendorPrefix 辨别浏览器内核]
-			 * @return {[type]} 浏览器内核['webkit','khtml','moz','ms','o']
-			 */
-			getVendorPrefix:function  () {
-				var body = document.body||document.documentElement,
-				style = body.style,
-				vendor = ['webkit','khtml','moz','ms','o'],
-				i = 0;
+				init: function() {
+					$tools.methods.D = $tools.methods.getVendorPrefix();
+					$tools.methods.run();
+				},
+				/**
+				 * [getVendorPrefix 辨别浏览器内核]
+				 * @return {[type]} 浏览器内核['webkit','khtml','moz','ms','o']
+				 */
+				getVendorPrefix: function() {
+					var body = document.body || document.documentElement,
+						style = body.style,
+						vendor = ['webkit', 'khtml', 'moz', 'ms', 'o'],
+						i = 0;
 
-				while(i < vendor.length){
-					if (typeof style[vendor[i] + 'Transition'] === 'string') {
-						return vendor[i];
+					while (i < vendor.length) {
+						if (typeof style[vendor[i] + 'Transition'] === 'string') {
+							return vendor[i];
+						};
+						i++;
+					}
+				},
+				run: function() {
+					try {
+						new $tools.marquee($('[data-action=Marquee]'), {
+							isEqual: 'false',
+							direction: 'left'
+						})
+					} catch (e) {
+						console.log('miss function && options is mistake')
 					};
-					i++;
-				}
-			},
-			run:function  () {
-				try{new $tools.marquee($('[data-action=Marquee]'),{isEqual:'false',direction:'left'})}catch(e){console.log('miss function && options is mistake')};
-				console.log('<style type="text/css">\n\t.exampleMarquee{\n\t\twidth: 100%;\n\t\theight: 30px;\n\t\toverflow: hidden;\n\t\tbackground-color: #ccc;\n\t\t}\n\t.exampleMarquee > *{\n\t\tlist-style-type: none;\n\t\tpadding: 0px;\n\t\tmargin: 0px;\n\t}\n\t.exampleMarquee > ul li{\n\t\tfloat: left;\n\t\tpadding: 0 10px;\n\t\tline-height: 30px;\n\t}\n</style>');
-				console.log('<div class="exampleMarquee" data-action="Marquee">\n\t<ul>\n\t\t<li>postdicroticteratogenousluxuriantllej</li>\n\t\t<li>ducatocrystalloticillate</li>\n\t</ul>\n</div>')
+					console.log('<style type="text/css">\n\t.exampleMarquee{\n\t\twidth: 100%;\n\t\theight: 30px;\n\t\toverflow: hidden;\n\t\tbackground-color: #ccc;\n\t\t}\n\t.exampleMarquee > *{\n\t\tlist-style-type: none;\n\t\tpadding: 0px;\n\t\tmargin: 0px;\n\t}\n\t.exampleMarquee > ul li{\n\t\tfloat: left;\n\t\tpadding: 0 10px;\n\t\tline-height: 30px;\n\t}\n</style>');
+					console.log('<div class="exampleMarquee" data-action="Marquee">\n\t<ul>\n\t\t<li>postdicroticteratogenousluxuriantllej</li>\n\t\t<li>ducatocrystalloticillate</li>\n\t</ul>\n</div>')
 
+				}
 			}
-		}
-		// <div class="exampleMarquee" data-action="Marquee">
-		// 	<ul>
-		// 		<li>postdicroticteratogenousluxuriantllej</li>
-		// 		<li>ducatocrystalloticillate</li>
-		// 	</ul>
-		// </div>
-		/**
-		 * [marquee 文字自动轮播]
-		 * @param  {[type]} el   [dom]
-		 * @param  {[type]} opts [配置参数，不设则使用默认项]
-		 * new $tools.marquee($('[data-action=Marquee]'),{
-		 * 		isEqual:false,
-		 * 		direction:'top'
-		 * })
-		 */
-		$tools.marquee = function  (el,opts) {
+			/**
+			 * [marquee 文字自动轮播]
+			 * @param  {[type]} el   [dom]
+			 * @param  {[type]} opts [配置参数，不设则使用默认项]
+			 * new $tools.marquee($('[data-action=Marquee]'),{
+			 * 		isEqual:false,
+			 * 		direction:'top'
+			 * })
+			 */
+		$tools.marquee = function(el, opts) {
 			/**
 			 * [defaults 参数设置]
 			 * @type {isEqual:true||false} 滚动元素长宽是否相等
@@ -56,37 +58,38 @@
 			 */
 			var defaults = {
 				isEqual: opts.isEqual == 'false' || opts.isEqual == false ? false : true,
-				loop:opts.loop || 0,
-				direction:opts.direction || "left",
-				scrollAmount:opts.scrollAmount || 1,
-				scrollDelay:opts.scrollDelay || 20,
-				scrollSize:null,
-				scrollType:null,
-				scrollMain:$(el).get(0)
+				loop: opts.loop || 0,
+				direction: opts.direction || "left",
+				scrollAmount: opts.scrollAmount || 1,
+				scrollDelay: opts.scrollDelay || 20,
+				scrollSize: null,
+				scrollType: null,
+				scrollMain: $(el).get(0)
 			}
 
 			defaults.$el = $(el);
 			defaults.$ul = defaults.$el.children();
 			defaults.$li = defaults.$ul.children();
-			defaults.scrollType = (defaults.direction == "left" || defaults.direction == "right") ? 1 : 0 ;
-			defaults.$ul.css(defaults.scrollType?"width":"height",10000)
+			defaults.scrollType = (defaults.direction == "left" || defaults.direction == "right") ? 1 : 0;
+			defaults.$ul.css(defaults.scrollType ? "width" : "height", 10000)
 
 			if (defaults.isEqual) {
-				defaults.scrollSize = defaults.$li[defaults.scrollType?"outerWidth":"outerHeight"]() * defaults.$li.length;
-			}else{
-				defaults.$li.each(function  () {
-					defaults.scrollSize += $(this)[defaults.scrollType?"outerWidth":"outerHeight"]();
+				defaults.scrollSize = defaults.$li[defaults.scrollType ? "outerWidth" : "outerHeight"]() * defaults.$li.length;
+			} else {
+				defaults.$li.each(function() {
+					defaults.scrollSize += $(this)[defaults.scrollType ? "outerWidth" : "outerHeight"]();
 				})
 			}
 
-			defaults.$ul.append(defaults.$li.clone()).css(defaults.scrollType?"width":"height", defaults.scrollSize * 2);
-			
+			defaults.$ul.append(defaults.$li.clone()).css(defaults.scrollType ? "width" : "height", defaults.scrollSize * 2);
+
 			$tools.init(defaults);
 		}
 
-		$tools.init = function  (opts) {
+		$tools.init = function(opts) {
 			var numMoved = 0;
-			function scrollFunc (opts) {
+
+			function scrollFunc(opts) {
 				opts.scrollDir = (opts.direction == "left" || opts.direction == "right") ? "scrollLeft" : "scrollTop";
 				if (opts.loop > 0) {
 					numMoved += opts.scrollAmount;
@@ -97,13 +100,13 @@
 				};
 
 				if (opts.direction == "left" || opts.direction == "up") {
-					var newPos = opts.scrollMain[opts.scrollDir]+opts.scrollAmount;
+					var newPos = opts.scrollMain[opts.scrollDir] + opts.scrollAmount;
 					if (newPos >= opts.scrollSize) {
 						newPos -= opts.scrollSize
 					};
 					opts.scrollMain[opts.scrollDir] = newPos;
-				}else{
-					var newPos = opts.scrollMain[opts.scrollDir]-opts.scrollAmount;
+				} else {
+					var newPos = opts.scrollMain[opts.scrollDir] - opts.scrollAmount;
 					if (newPos <= 0) {
 						newPos += opts.scrollSize;
 					};
@@ -111,17 +114,17 @@
 				}
 			}
 
-			var Move = setInterval(function  () {
+			var Move = setInterval(function() {
 				scrollFunc(opts)
-			},opts.scrollDelay);
+			}, opts.scrollDelay);
 
 			opts.$el.hover(function() {
 				clearInterval(Move);
 			}, function() {
 				clearInterval(Move);
-				Move = setInterval(function  () {
+				Move = setInterval(function() {
 					scrollFunc(opts)
-				},opts.scrollDelay);
+				}, opts.scrollDelay);
 			});
 		}
 		return $tools;
@@ -129,7 +132,7 @@
 })(jQuery);
 
 var $tools = null;
-$(function  () {
+$(function() {
 	$tools = new $.tools();
 	$tools.methods.init();
 })
